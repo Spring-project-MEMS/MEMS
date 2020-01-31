4da8232721bd033a1275769f53fbb8ae152c789d
towny56 <a.t.todorova97@gmail.com>
2020-01-31 02:01:54 +0200

    Making functionality for visualisation all upcoming appointments
    Need to add restriction for user to see only his/hers appointments and for doctors to see only appointments in their ward.

--- Forum/src/main/java/com/fixit/areas/appointments/models/view/AppointmentViewModel.java
+++ Forum/src/main/java/com/fixit/areas/appointments/models/view/AppointmentViewModel.java
@@ -1,5 +1,6 @@
 package com.fixit.areas.appointments.models.view;
 
+import com.fixit.areas.users.models.view.UsersViewModel;
 import com.fixit.areas.ward.models.view.WardViewModel;
 
 import java.util.Date;
@@ -10,12 +11,12 @@ public class AppointmentViewModel {
     private String date;
     private String time;
 
+    private UsersViewModel patient;
     private WardViewModel ward;
 
     // TODO:
     // will add these fields once the view models are done
     /*
-    private UserViewModel patient;
     private ExaminationViewModel examinations;
     */
 
@@ -44,6 +45,14 @@ public class AppointmentViewModel {
 
     public void setTime(String time) {
         this.time = time;
+    }
+
+    public UsersViewModel getPatient() {
+        return patient;
+    }
+
+    public void setPatient(UsersViewModel patient) {
+        this.patient = patient;
     }
 
     public WardViewModel getWard() {
